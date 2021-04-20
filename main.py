6d3bd69df94d5f1c103f93e0c816342553fab94d
artemka1647 <78467756+artemka1647@users.noreply.github.com>
2021-04-20 23:29:19 +0500

    1.24

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Mentor:
         self.courses_attached.append(course)
 
 class Student:
+    ave_grade = float
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -18,20 +19,24 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         self.grades_list = []
-        self.ave_grade = float
+
     def __str__(self):
-        return f"Имя:{self.name} ,Фамилия:{self.surname},Средняя оценка за домашние задания :{self.ave_grade}, Изучаемые курсы:{self.courses_in_progress}, Изученные курсы:{self.finished_courses}"
+        text = (f"Имя:{self.name}"
+                f" Фамилия:{self.surname}" 
+                f" Средняя оценка за домашние задания :{self.ave_grade}"
+                f" Изучаемые курсы:{self.courses_in_progress},"
+                f" Изученные курсы:{self.finished_courses}")
+        return text
 
-
-    def add_fin_course(self,course):
+    def add_fin_course(self, course):
         self.finished_courses.append(course)
 
-    def add_progress_courses(self,course):
+    def add_progress_courses(self, course):
         self.courses_in_progress.append(course)
 
 
 
-    def rate_lect(self, lecturer, course,digit):
+    def rate_lect(self, lecturer, course, digit):
 
         if isinstance(lecturer, Lecturer):
             if course in self.courses_in_progress and course in lecturer.course_lect:
@@ -43,9 +48,9 @@ class Student:
             raise ('Это не лектор')
 
 
-    def average_grade(self, student):
-        self.ave_grade = sum(student.grades.values())/len(student.grades.keys())
-
+    def average_grade(self):
+        ave_grade = sum(self.grades_list)/len(self.grades_list)
+        return ave_grade
 
 
 class Reviewer(Mentor):
@@ -65,21 +70,22 @@ class Reviewer(Mentor):
         return f"Имя:{self.name} ,Фамилия:{self.surname}"
 
 class Lecturer(Mentor):
-
-    def __init__(self,name,surname):
+    ave_rate = float
+    def __init__ (self, name, surname,):
         self.name = name
         self.surname = surname
         self.rate = []
         self.course_lect = []
-        self.ave_rate = None
+
     def __str__(self):
         return f"Имя:{self.name} ,Фамилия:{self.surname},Средняя оценка за лекции :{self.ave_rate}"
 
-    def add_cour_lect(self,course):
+    def add_cour_lect(self, course):
         self.course_lect.append(course)
 
     def add_ave_rate(self):
-        self.ave_rate = sum(self.rate)/ len(self.rate)
+        ave_rate = (sum(self.rate))/len(self.rate)
+        return ave_rate
 
 
 
@@ -102,7 +108,7 @@ student2.grades['Зельеварение'] = None
 student2.grades['Защита от темных искусств'] = None
 student_list.append(student2)
 pprint(student1.__str__())
-pprint(student2.__str__())
+#pprint(student2.__str__())
 
 
 lecturer1 = Lecturer('Филиас','Флитвик')
@@ -112,8 +118,8 @@ lecturer2.add_cour_lect("Трансфигурация")
 lecturer_list = []
 lecturer_list.append(lecturer1)
 lecturer_list.append(lecturer2)
-pprint(lecturer1.__str__())
-pprint(lecturer2.__str__())
+#pprint(lecturer1.__str__())
+#pprint(lecturer2.__str__())
 
 student1.rate_lect(lecturer1, "Заклинания", 9)
 student1.rate_lect(lecturer2, "Трансфигурация", 10)
@@ -122,14 +128,14 @@ student2.rate_lect(lecturer2, "Трансфигурация", 10)
 
 
 reviewer1 = Reviewer('Альбус', 'Дамблдор')
-reviewer2 = Reviewer('Северус','Снегг')
+reviewer2 = Reviewer('Северус', 'Снегг')
 reviewer2.add_cour_attached('Зельеварение')
 reviewer1.add_cour_attached('Защита от темных искусств')
 reviewer1.rate_hw(student1, "Защита от темных искусств", 10)
 reviewer1.rate_hw(student2, "Защита от темных искусств", 9)
-reviewer2.rate_hw( student1, "Зельеварение",6)
-reviewer2.rate_hw( student2, "Зельеварение",7)
-pprint(reviewer1.__str__())
-pprint(reviewer2.__str__())
+reviewer2.rate_hw( student1, "Зельеварение", 6)
+reviewer2.rate_hw( student2, "Зельеварение", 7)
+#pprint(reviewer1.__str__())
+#pprint(reviewer2.__str__())
 
 
